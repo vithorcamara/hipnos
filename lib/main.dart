98816335ed285b08c4b562f66106a6c876f49182
vithor_camara <vitor.camarapd@gmail.com>
2024-06-07 19:37:46 +0000

    Update FlutLab #1

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,17 @@
 import 'package:flutter/material.dart';
+import 'splashScreen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const Main());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Main extends StatelessWidget {
+  const Main({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/splashScreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+// ignore: use_key_in_widget_constructors
+class SplashScreen extends StatefulWidget {
+  @override
+  // ignore: library_private_types_in_public_api
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('../images/Prometeus-Logo4.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(),
+      ),
+    );
+  }
+}
